ebfb3a8f95220d26f7de7edbf2b85d9787aa6b42
chrl0t <charlotte_i@live.co.uk>
2021-03-04 14:29:25 +0000

    updated forage card to use state

--- src/components/ForageCard.js
+++ src/components/ForageCard.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 
-const ForageCard = () => {
+const ForageCard = ({ navigation }) => {
+  const [name, setName] = useState("Wild Garlic");
+  const [availability, setAvailability] = useState("March - May");
+  const [locations, setLocations] = useState("Cemeteries, Parks, Woodland");
+  const [warnings, setWarnings] = useState(
+    "This plant can grow alongside a poisonous one called Lords and Ladies"
+  );
+  console.log(navigation);
   return (
     <View style={styles.container}>
       <View style={styles.infoContainer}>
-        <Text style={styles.title}>Wild Garlic</Text>
+        <Text style={styles.title}>{name}</Text>
         <Text style={styles.heading}>Available:</Text>
-        <Text style={styles.info}>March - May</Text>
+        <Text style={styles.info}>{availability}</Text>
         <Text style={styles.heading}>Where:</Text>
-        <Text style={styles.info}>Cemeteries, Parks, Woodland</Text>
+        <Text style={styles.info}>{locations}</Text>
         <Text style={styles.heading}>Caution:</Text>
-        <Text style={styles.info}>
-          This plant can grow alongside a poisonous one called Lords and Ladies
-        </Text>
+        <Text style={styles.info}>{warnings}</Text>
       </View>
       <Image
         style={styles.image}
@@ -47,11 +52,13 @@ const styles = StyleSheet.create({
   heading: {
     fontFamily: "KohinoorDevanagari-Light",
     fontSize: 20,
-    fontWeight: "400"
+    fontWeight: "400",
+    padding: 3
   },
   info: {
     fontFamily: "KohinoorDevanagari-Light",
-    fontSize: 12
+    fontSize: 12,
+    paddingLeft: 3
   },
   infoContainer: {
     flexDirection: "column",
