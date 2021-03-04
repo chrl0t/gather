814c92ff5bbe62994d450a471004e4c708b642ce
chrl0t <charlotte_i@live.co.uk>
2021-03-04 12:27:44 +0000

    created forage card component

---
+++ src/components/ForageCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ForageCard = () => {
+  return (
+    <View>
+      <Text>Forage Card</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ForageCard;
