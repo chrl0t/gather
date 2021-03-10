b25a75f012bd8cff76dbfd02fd47f50d81103180
chrl0t <charlotte_i@live.co.uk>
2021-03-10 14:59:53 +0000

    removed determineSeason function from header component and imported from utils

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { determineSeason } from "../utils/check-season";
 
 const Header = () => {
   const [season, setSeason] = useState("");
 
   const date = new Date();
   const month = date.getMonth();
-
-  const determineSeason = (passedMonth) => {
-    let season = "";
-    if (passedMonth === 11 || passedMonth === 0 || passedMonth === 1) {
-      season += "Winter";
-    } else if (passedMonth === 2 || passedMonth === 3 || passedMonth === 4) {
-      season += "Spring";
-    } else if (passedMonth === 5 || passedMonth === 6 || passedMonth === 7) {
-      season += "Summer";
-    } else if (passedMonth === 8 || passedMonth === 9 || passedMonth === 10) {
-      season += "Autumn";
-    }
-    return season;
-  };
 
   useEffect(() => {
     setSeason(determineSeason(month));
