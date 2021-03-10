1c7673bebf18ec6c68d340a581e40817d56bc672
chrl0t <charlotte_i@live.co.uk>
2021-03-10 14:57:07 +0000

    wrote and passed tests for determine season function

--- src/utils/check-season.js
+++ src/utils/check-season.js
@@ -1,12 +1,12 @@
 export const determineSeason = (month) => {
   let season = "";
-  if (passedMonth === 11 || passedMonth === 0 || passedMonth === 1) {
+  if (month === 11 || month === 0 || month === 1) {
     season += "Winter";
-  } else if (passedMonth === 2 || passedMonth === 3 || passedMonth === 4) {
+  } else if (month === 2 || month === 3 || month === 4) {
     season += "Spring";
-  } else if (passedMonth === 5 || passedMonth === 6 || passedMonth === 7) {
+  } else if (month === 5 || month === 6 || month === 7) {
     season += "Summer";
-  } else if (passedMonth === 8 || passedMonth === 9 || passedMonth === 10) {
+  } else if (month === 8 || month === 9 || month === 10) {
     season += "Autumn";
   }
   return season;
---
+++ src/utils/check-season.test.js
@@ -0,0 +1,28 @@
+const { determineSeason } = require("./check-season");
+
+describe("determineSeason()", () => {
+  it("should return winter if a winter month is passed in", () => {
+    const input = 0;
+    const output = determineSeason(input);
+    const expectedOutput = "Winter";
+    expect(output).toEqual(expectedOutput);
+  });
+  it("should return spring if a spring month is passed in", () => {
+    const input = 2;
+    const output = determineSeason(input);
+    const expectedOutput = "Spring";
+    expect(output).toEqual(expectedOutput);
+  });
+  it("should return summer if a summer month is passed in", () => {
+    const input = 5;
+    const output = determineSeason(input);
+    const expectedOutput = "Summer";
+    expect(output).toEqual(expectedOutput);
+  });
+  it("should return autumn if a autumn month is passed in", () => {
+    const input = 8;
+    const output = determineSeason(input);
+    const expectedOutput = "Autumn";
+    expect(output).toEqual(expectedOutput);
+  });
+});
