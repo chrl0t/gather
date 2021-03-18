12ff6b7db6c5bcd3fc30fbe797c430b1baff00fa
chrl0t <charlotte_i@live.co.uk>
2021-03-18 15:59:08 +0000

    fetching specific item in forage screen and displaying info

--- src/screens/ForageScreen.js
+++ src/screens/ForageScreen.js
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
+import { useEffect } from "react/cjs/react.development";
+import { db } from "../firebase/config";
 
 const ForageScreen = ({ navigation }) => {
-  const [name, setName] = useState("Wild Garlic");
-  const [latin, setLatin] = useState("Allium ursinum");
-  const [description, setDescription] = useState(
-    "Quite possibly the Godfather of all wild greens, the Steve Jobs of the onion family, The Beatles of foraged foods, wild garlic is the one to find as it's delicious, vibrant and versatile. A keen nose won't have any difficulty in sniffing out this plant's heady onion/garlic aroma. It always seems to grow in the most beautiful areas too, making it a joy to go looking for."
-  );
-  const [identification, setIdentification] = useState(
-    "Ranging from 2-30 centimetres in height, wild garlic's bright green tapered leaves are best picked by the stem a coupole of centimetres up from the ground. Be careful not to uproot it, as you'll definitely want to return to the same spot to pick a few of its tall white flower heads for a salad garnish when they start to bloom, and even collect some green, spherical seed pod clusters at the end of the season for pickling."
-  );
+  const [name, setName] = useState("");
+  const [latin, setLatin] = useState("");
+  const [description, setDescription] = useState("");
+  const [identification, setIdentification] = useState("");
   const id = navigation.getParam("id");
+
+  useEffect(() => {
+    async function fetchData() {
+      const foragesRef = db.collection("forages").where("id", "==", id);
+      const snapshot = await foragesRef.get();
+      let forageInfo = {};
+      snapshot.forEach((doc) => {
+        forageInfo = doc.data();
+      });
+      setName(forageInfo.name);
+      setLatin(forageInfo.latin);
+      setDescription(forageInfo.description);
+      setIdentification(forageInfo.identification);
+    }
+    fetchData();
+  }, []);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{name}</Text>
