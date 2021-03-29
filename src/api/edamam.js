fac9b9b68d594cf2f2c7e12fdc8dc0e1b0dcb859
chrl0t <charlotte_i@live.co.uk>
2021-03-29 16:33:18 +0100

    installed rn package and passed all necessary fetched info to recipes screen

--- src/api/edamam.js
+++ src/api/edamam.js
@@ -1,11 +1,18 @@
 import axios from "axios";
 
-export const fetchRecipes = (ingredient) => {
+export const fetchRecipes = (
+  ingredient,
+  setRecipeTitle,
+  setRecipeImage,
+  setRecipeLink
+) => {
   return axios
     .get(
       `https://api.edamam.com/search?q=${ingredient}&app_id=df1d6767&app_key=6b2c1871ed0727f6939f433b36c752f9`
     )
     .then(({ data }) => {
-      console.log(data.hits[0].recipe.label, "<-this");
+      setRecipeTitle(data.hits[0].recipe.label);
+      setRecipeImage(data.hits[0].recipe.image);
+      setRecipeLink(data.hits[0].recipe.url);
     });
 };
--- src/screens/RecipesScreen.js
+++ src/screens/RecipesScreen.js
@@ -1,18 +1,41 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { mutateName } from "../utils/mutate-name";
+import { fetchRecipes } from "../api/edamam";
+import Communications from "react-native-communications";
 
 const RecipesScreen = ({ navigation }) => {
+  const [recipeTitle, setRecipeTitle] = useState("");
+  const [recipeImage, setRecipeImage] = useState(
+    "https://cdn.cnn.com/cnnnext/dam/assets/200319162747-family-cooking-stock-super-tease.jpg"
+  );
+  const [recipeLink, setRecipeLink] = useState("");
   const name = navigation.getParam("name");
-  const fetchName = mutateName(name);
+  const ingredient = mutateName(name);
+
+  useEffect(() => {
+    fetchRecipes(ingredient, setRecipeTitle, setRecipeImage, setRecipeLink);
+  }, []);
   return (
     <View>
       <Text>{name}</Text>
-      <Text>{fetchName}</Text>
+      <Text>{ingredient}</Text>
+      <Text>{recipeTitle}</Text>
+      <Image style={styles.image} source={{ uri: recipeImage }} />
+      <TouchableOpacity onPress={() => Communications.web(recipeLink)}>
+        <Text>Click here for full recipe</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 270,
+    alignSelf: "center",
+    margin: 15
+  }
+});
 
 export default RecipesScreen;
