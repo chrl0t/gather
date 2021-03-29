67855483627faaff025717f96fc64ae4ff6c4ddf
chrl0t <charlotte_i@live.co.uk>
2021-03-29 16:08:53 +0100

    added fetch recipe function to api file using edamam api

---
+++ src/api/edamam.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const fetchRecipes = (ingredient) => {
+  return axios
+    .get(
+      `https://api.edamam.com/search?q=${ingredient}&app_id=df1d6767&app_key=6b2c1871ed0727f6939f433b36c752f9`
+    )
+    .then(({ data }) => {
+      console.log(data.hits[0].recipe.label, "<-this");
+    });
+};
