3ec85020f057b0413eb4aee12edd3ad0f18c2515
chrl0t <charlotte_i@live.co.uk>
2021-04-12 14:19:08 +0100

    Create Saved Forages Screen

---
+++ src/screens/SavedForagesScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SavedForagesScreen = () => {
+  return (
+    <View>
+      <Text>Saved Forages Screen</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
+
+export default SavedForagesScreen;
