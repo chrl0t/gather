7fffee0931795308d8f8b35d0476cbc71ee19007
chrl0t <charlotte_i@live.co.uk>
2021-04-12 14:11:15 +0100

    Create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Footer = () => {
+  return (
+    <View>
+      <Text>Footer</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Footer;
