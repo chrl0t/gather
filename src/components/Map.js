754cb101aa331f6326c1880d0c388daa7b33b796
chrl0t <charlotte_i@live.co.uk>
2021-04-13 13:53:57 +0100

    Add map to map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 
 const Map = () => {
-  return <Text>Map here</Text>;
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 54.5496,
+        longitude: -1.2509,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+    />
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300
+  }
+});
 
 export default Map;
