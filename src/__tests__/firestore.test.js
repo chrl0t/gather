a9be731f5d795f3eadb624e89798f89ae92d022f
chrl0t <charlotte_i@live.co.uk>
2021-04-26 15:25:55 +0100

    Write test for fetchForage

--- src/__tests__/firestore.test.js
+++ src/__tests__/firestore.test.js
@@ -11,3 +11,13 @@ describe("fetchListOfForages()", () => {
     expect(firstResult).toEqual(expect.arrayContaining(expected));
   });
 });
+
+describe("fetchForage()", () => {
+  it("should return an object of the foragable item with the correct properties", async () => {
+    const response = await fetchForage(1);
+    expect(response).toHaveProperty("name");
+    expect(response).toHaveProperty("latin");
+    expect(response).toHaveProperty("description");
+    expect(response).toHaveProperty("identification");
+  });
+});
